8c07f5b7f2b3f161ff9c22bb41abfd11ff188e1d
Stefan Lau <github@stefanlau.com>
2013-10-30 19:38:05 +0100

    handle non 200 status codes in blockchainInfo module

--- test/specs/lib/modules/technical/blockchainInfoSpec.js
+++ test/specs/lib/modules/technical/blockchainInfoSpec.js
@@ -17,9 +17,7 @@ var _ = require('lodash'),
                 var response = responses[options.uri],
                     triggerResponse = function (err, response) {
                         setTimeout(function () {
-                            callback(err, {
-                                body: response
-                            });
+                            callback(err, response);
                         }, 20);
                     };
 
@@ -37,9 +35,18 @@ describe('modules/technical/blockchainInfo', function () {
 
     beforeEach(function () {
         responses = {
-            'http://blockchain.info/de/stats?format=json': statsAnswer,
-            'http://blockchain.info/de/q/bcperblock': btcPerBlockAnswer,
-            'http://blockchain.info/de/q/probability': probabilityAnswer
+            'http://blockchain.info/de/stats?format=json': {
+                statusCode: 200,
+                body: statsAnswer
+            },
+            'http://blockchain.info/de/q/bcperblock': {
+                statusCode: 200,
+                body: btcPerBlockAnswer
+            },
+            'http://blockchain.info/de/q/probability': {
+                statusCode: 200,
+                body: probabilityAnswer
+            }
         };
     });
 
@@ -89,6 +96,57 @@ describe('modules/technical/blockchainInfo', function () {
             blockchainInfo;
 
         responses['http://blockchain.info/de/q/probability'] = null;
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
+    });
+
+    it('should not throw an error if the stats request fails with a non 200 status code', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockchain.info/de/stats?format=json'] = {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
+    });
+
+    it('should not throw an error if the bcperblock request fails with a non 200 status code', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockchain.info/de/q/bcperblock'] = {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
+    });
+
+    it('should not throw an error if the probability request fails with a non 200 status code', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockchain.info/de/q/probability'] = {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
 
         blockchainInfo = new BlockchainInfo(app);
 
