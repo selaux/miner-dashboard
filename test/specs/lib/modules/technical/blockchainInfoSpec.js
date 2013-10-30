f22f09939d9b3cccaaf70a15b33cc4a017795f02
Stefan Lau <github@stefanlau.com>
2013-10-30 19:03:34 +0100

    handle errors from requests tp blockchain.info

--- test/specs/lib/modules/technical/blockchainInfoSpec.js
+++ test/specs/lib/modules/technical/blockchainInfoSpec.js
@@ -5,6 +5,7 @@ var _ = require('lodash'),
     expect = chai.expect,
     SandboxedModule = require('sandboxed-module'),
 
+    responses,
     statsAnswer = {
         some: 'stats'
     },
@@ -13,33 +14,34 @@ var _ = require('lodash'),
     BlockchainInfo = SandboxedModule.require('../../../../../lib/modules/technical/blockchainInfo', {
         requires: {
             'request': function (options, callback) {
-                var response;
+                var response = responses[options.uri],
+                    triggerResponse = function (err, response) {
+                        setTimeout(function () {
+                            callback(err, {
+                                body: response
+                            });
+                        }, 20);
+                    };
 
                 expect(options.json).to.be.true;
-                switch (options.uri) {
-                case 'http://blockchain.info/de/stats?format=json':
-                    response = statsAnswer;
-                    break;
-                case 'http://blockexplorer.com/q/bcperblock':
-                    response = btcPerBlockAnswer;
-                    break;
-                case 'http://blockchain.info/de/q/probability':
-                    response = probabilityAnswer;
-                    break;
-                default:
-                    throw new Error('Unmatched uri: ' + options.uri);
+                if (response) {
+                    triggerResponse(null, response);
+                } else {
+                    triggerResponse(new Error('Test Error'));
                 }
-
-                setTimeout(function () {
-                    callback(null, {
-                        body: response
-                    });
-                }, 20);
             }
         }
     });
 
 describe('modules/technical/blockchainInfo', function () {
+
+    beforeEach(function () {
+        responses = {
+            'http://blockchain.info/de/stats?format=json': statsAnswer,
+            'http://blockexplorer.com/q/bcperblock': btcPerBlockAnswer,
+            'http://blockchain.info/de/q/probability': probabilityAnswer
+        };
+    });
 
     it('should get data from blockchainInfo correctly', function (done) {
         var app = {},
@@ -52,6 +54,48 @@ describe('modules/technical/blockchainInfo', function () {
             }));
             done();
         });
+    });
+
+    it('should not throw an error if the stats request fails with an error', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockchain.info/de/stats?format=json'] = null;
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
+    });
+
+    it('should not throw an error if the bcperblock request fails with an error', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockexplorer.com/q/bcperblock'] = null;
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
+    });
+
+    it('should not throw an error if the probability request fails with an error', function (done) {
+        var app = {},
+            blockchainInfo;
+
+        responses['http://blockchain.info/de/q/probability'] = null;
+
+        blockchainInfo = new BlockchainInfo(app);
+
+        setTimeout(function () {
+            expect(blockchainInfo.data).not.to.be.ok;
+            done();
+        }, 50);
     });
 
     it('should have the title set correctly', function () {
