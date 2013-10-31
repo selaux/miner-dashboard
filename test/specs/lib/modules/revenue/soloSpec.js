e790a067660da66f75e73f5d69dba826ec1fd5eb
Stefan Lau <github@stefanlau.com>
2013-10-31 17:16:09 +0100

    cleanup solo revenue test

--- test/specs/lib/modules/revenue/soloSpec.js
+++ test/specs/lib/modules/revenue/soloSpec.js
@@ -8,30 +8,23 @@ var EventEmitter = require('events').EventEmitter,
     Solo = require('../../../../../lib/modules/revenue/solo');
 
 describe('modules/revenue/solo', function () {
+    var defaultConfig = {
+        miner: 'minerId',
+        market: 'marketId',
+        technical: 'technicalId'
+    };
 
-    function setUp () {
-        var app = new EventEmitter(),
-            config = {
-                miner: 'minerId',
-                market: 'marketId',
-                technical: 'technicalId'
-            },
-            solo = new Solo(app, config);
-
-        return {
-            app: app,
-            module: solo
-        };
-    }
+    beforeEach(function () {
+        this.app = new EventEmitter();
+    });
 
     it('should calculate revenue when any miner hashrate is updated', function (done) {
-        var app = new EventEmitter(),
-            config = {
+        var config = {
                 miner: [ 'minerId1', 'minerId2' ],
                 market: 'marketId',
                 technical: 'technicalId'
             },
-            solo = new Solo(app, config);
+            solo = new Solo(this.app, config);
 
         solo.minerData = {
             minerId1: { avgHashrate: 0.5 * 1e-6 }
@@ -48,13 +41,11 @@ describe('modules/revenue/solo', function () {
             done();
         });
 
-        app.emit('update:data:minerId2', { avgHashrate: 0.5 * 1e-6 });
+        this.app.emit('update:data:minerId2', { avgHashrate: 0.5 * 1e-6 });
     });
 
     it('should calculate revenue when miner is not connected', function (done) {
-        var setup = setUp(),
-            app = setup.app,
-            solo = setup.module;
+        var solo = new Solo(this.app, defaultConfig);
 
         solo.marketData = { ask: 100, currency: 'NMC' };
         solo.technicalData = { btcPerBlock: 10, probability: 0.0001 };
@@ -68,13 +59,11 @@ describe('modules/revenue/solo', function () {
             done();
         });
 
-        app.emit('update:data:minerId', { connected: false });
+        this.app.emit('update:data:minerId', { connected: false });
     });
 
     it('should calculate revenue when market prices are updated', function (done) {
-        var setup = setUp(),
-            app = setup.app,
-            solo = setup.module;
+        var solo = new Solo(this.app, defaultConfig);
 
         solo.minerData = { minerId: { avgHashrate: 1e-6 } };
         solo.technicalData = { btcPerBlock: 10, probability: 0.0001 };
@@ -88,13 +77,11 @@ describe('modules/revenue/solo', function () {
             done();
         });
 
-        app.emit('update:data:marketId', { ask: 100, currency: 'NMC' });
+        this.app.emit('update:data:marketId', { ask: 100, currency: 'NMC' });
     });
 
     it('should calculate revenue when technical info is updated', function (done) {
-        var setup = setUp(),
-            app = setup.app,
-            solo = setup.module;
+        var solo = new Solo(this.app, defaultConfig);
 
         solo.minerData = { minerId: { avgHashrate: 1e-6 } };
         solo.marketData = { ask: 100, currency: 'NMC' };
@@ -108,26 +95,18 @@ describe('modules/revenue/solo', function () {
             done();
         });
 
-        app.emit('update:data:technicalId', { btcPerBlock: 10, probability: 0.0001 });
+        this.app.emit('update:data:technicalId', { btcPerBlock: 10, probability: 0.0001 });
     });
 
     it('should set the title to "Revenue" when no title is set in config', function () {
-        var app = {
-                on: function () {}
-            },
-            solo = new Solo(app, {});
-
+        var solo = new Solo(this.app, {});
         expect(solo.title).to.equal('Revenue');
     });
 
     it('should set the title to config.title when it is set', function () {
-        var app = {
-                on: function () {}
-            },
-            solo = new Solo(app, {
+        var solo = new Solo(this.app, {
                 title: 'Some Title'
             });
-
         expect(solo.title).to.equal('Some Title');
     });
 
