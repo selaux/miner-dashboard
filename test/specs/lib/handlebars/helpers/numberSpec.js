bcf17729d9f779b42f9665896a0cfd267e0dae33
Stefan Lau <github@stefanlau.com>
2013-11-03 15:38:47 +0100

    use handlebars helper to format numbers

---
+++ test/specs/lib/handlebars/helpers/numberSpec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var chai = require('chai'),
+    expect = chai.expect,
+
+    number = require('../../../../../lib/handlebars/helpers/number');
+
+describe('handlebars/helpers/number', function () {
+
+    [
+        { number: undefined, expected: '' },
+        { number: 'a string', expected: 'a string' },
+        { number: 0, expected: '0' },
+        { number: 10, expected: '10' },
+        { number: 10.31, expected: '10.31' },
+        { number: 10.312667, expected: '10.31267' },
+        { number: 10.312667, precision: 3, expected: '10.313' },
+        { number: 1000, expected: '1,000' },
+        { number: 1000.132561, expected: '1,000.13256' },
+        { number: 1000.132561, precision: 3, expected: '1,000.133' },
+    ].forEach(function (testCase) {
+        it('should return "' + testCase.expected + '" for "' + testCase.number + '"', function () {
+            var options = { hash: {} };
+
+            if (testCase.precision) {
+                options.hash.precision = testCase.precision.toString();
+            }
+
+            expect(number(testCase.number, options)).to.equal(testCase.expected);
+        });
+    });
+
+});
