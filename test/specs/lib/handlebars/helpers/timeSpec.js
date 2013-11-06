efd3afff810f86dfe1bf4d01ec42d8529ac6e9e2
Stefan Lau <github@stefanlau.com>
2013-11-06 17:48:54 +0100

    add time handlebars helper

---
+++ test/specs/lib/handlebars/helpers/timeSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var moment = require('moment'),
+    chai = require('chai'),
+    expect = chai.expect,
+
+    time = require('../../../../../lib/handlebars/helpers/time');
+
+describe('handlebars/helpers/time', function () {
+
+    [
+        { time: undefined, expected: '' },
+        { time: 'a string', expected: 'a string' },
+        { time: moment('2013-02-08 09:30').toDate(), expected: '2013-02-08, 09:30:00' },
+        { time: moment('2013-02-08 20:30:11').toDate(), expected: '2013-02-08, 20:30:11' }
+    ].forEach(function (testCase) {
+        it('should return "' + testCase.expected + '" for "' + testCase.time + '"', function () {
+            expect(time(testCase.time)).to.equal(testCase.expected);
+        });
+    });
+
+});
