bb54432c64538c9394e16dfa7949e2d40257ca5f
Stefan Lau <github@stefanlau.com>
2013-12-06 15:12:31 +0100

    make bin file usable with global npm installation, closes #21

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
+
 'use strict';
 
-var _ = require('lodash'),
-    config = require('./config/config.js'),
+var path = require('path'),
+    _ = require('lodash'),
+    opts = require('nomnom'),
+    config,
     webinterface = require('./frontend/webinterface'),
     webinterfaceDefaults = {
         id: '__webinterface__',
@@ -10,6 +14,14 @@ var _ = require('lodash'),
     },
     App = require('./lib/App');
 
+
+opts = opts.option('config', {
+    abbr: 'c',
+    'default': './config/config.js',
+    help: 'Path to config file'
+}).parse();
+
+config = require(path.resolve(opts.config));
 config.modules.push(_.extend(webinterfaceDefaults, config.webinterface));
 
 module.exports = new App(config);
