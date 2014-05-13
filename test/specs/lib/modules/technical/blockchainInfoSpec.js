c6e74c33c468c476079c4c2b08bdf9c1faecc28b
Stefan Lau <github@stefanlau.com>
2014-05-13 18:05:44 +0200

    Use promised request for blockchainInfo module

--- test/specs/lib/modules/technical/blockchainInfoSpec.js
+++ test/specs/lib/modules/technical/blockchainInfoSpec.js
@@ -5,8 +5,8 @@ var chai = require('chai'),
     sinonChai = require('sinon-chai'),
     expect = chai.expect,
     SandboxedModule = require('sandboxed-module'),
+    Bluebird = require('bluebird'),
 
-    responses = {},
     statsAnswer = {
         'n_blocks_total': 123,
         'minutes_between_blocks': 12,
@@ -15,90 +15,75 @@ var chai = require('chai'),
         difficulty: 100 * (1 / 4295032833),
         'hash_rate': 123
     },
-    btcPerBlockAnswer = 2500000000,
-    BlockchainInfo = SandboxedModule.require('../../../../../lib/modules/technical/blockchainInfo', {
-        requires: {
-            request: require('../../../../utils/requestStub')(responses)
-        }
-    });
+    btcPerBlockAnswer = 2500000000;
 
 chai.use(sinonChai);
 
 describe('modules/technical/blockchainInfo', function () {
-    var app;
+    var BlockchainInfo,
+        app,
+        requestStub;
 
     beforeEach(function () {
+        requestStub = sinon.stub();
+        requestStub.withArgs('https://blockchain.info/stats?format=json').returns(Bluebird.resolve(statsAnswer));
+        requestStub.withArgs('https://blockchain.info/q/bcperblock').returns(Bluebird.resolve(btcPerBlockAnswer));
+
+        BlockchainInfo = SandboxedModule.require('../../../../../lib/modules/technical/blockchainInfo', {
+            requires: {
+                '../../utils/request': requestStub
+            }
+        });
         app = { logger: { debug: sinon.stub(), info: sinon.stub() } };
-        responses['http://blockchain.info/stats?format=json'] = {
-            statusCode: 200,
-            body: statsAnswer
-        };
-        responses['http://blockchain.info/q/bcperblock'] = {
-            statusCode: 200,
-            body: btcPerBlockAnswer
-        };
     });
 
     it('should get data from blockchainInfo correctly', function (done) {
-        var blockchainInfo = new BlockchainInfo(app);
+        var blockchainInfo;
 
+        blockchainInfo = new BlockchainInfo(app);
         blockchainInfo.on('change', function () {
-            expect(blockchainInfo.toJSON()).to.deep.equal({
-                blockReward: 25,
-                probability: 0.01,
-                difficulty: 100 * (1 / 4295032833),
-                networkHashrate: 123000,
-                blockChainLength: 123,
-                timeBetweenBlocks: 12,
-                numberOfTransactions: 10,
-                totalTransactionValue: 1
+            setImmediate(function () {
+                expect(blockchainInfo.toJSON()).to.deep.equal({
+                    blockReward: 25,
+                    probability: 0.01,
+                    difficulty: 100 * (1 / 4295032833),
+                    networkHashrate: 123000,
+                    blockChainLength: 123,
+                    timeBetweenBlocks: 12,
+                    numberOfTransactions: 10,
+                    totalTransactionValue: 1
+                });
+
+                expect(app.logger.debug).to.have.been.calledOnce;
+                expect(app.logger.debug).to.have.been.calledWith(
+                    '%s - fetched data from blockchain.info',
+                    blockchainInfo.id,
+                    JSON.stringify([ statsAnswer, btcPerBlockAnswer ])
+                );
+
+                done();
             });
-            expect(app.logger.debug).to.have.been.calledOnce;
-            expect(app.logger.debug).to.have.been.calledWith(
-                '%s - fetched data from blockchain.info',
-                blockchainInfo.id,
-                JSON.stringify([ 200, 200 ]),
-                JSON.stringify([ statsAnswer, btcPerBlockAnswer ])
-            );
-            done();
         });
     });
 
     [
-        'http://blockchain.info/stats?format=json',
-        'http://blockchain.info/q/bcperblock'
-    ].forEach(function (url) {
-        it('should not throw an error if the request to ' + url + 'fails with an error and log the incident', function (done) {
-            var blockchainInfo;
+        'https://blockchain.info/stats?format=json',
+        'https://blockchain.info/q/bcperblock'
+    ].forEach(function (testCase) {
+        it('should log the incident if the request to ' + testCase.failing + 'fails with an error', function (done) {
+            var error = new Error('Test Error'),
+                blockchainInfo;
 
-            responses[url] = null;
+            requestStub.withArgs(testCase).returns(Bluebird.reject(error));
 
             blockchainInfo = new BlockchainInfo(app);
-
             setTimeout(function () {
                 expect(blockchainInfo.toJSON()).to.be.empty;
                 expect(app.logger.info).to.have.been.calledOnce;
-                expect(app.logger.info).to.have.been.calledWith('%s - error fetching data from blockchain.info', blockchainInfo.id, new Error('Test Error'));
+                expect(app.logger.info).to.have.been.calledWith('%s - error fetching data from blockchain.info', blockchainInfo.id, error.toString());
                 done();
-            }, 50);
+            }, 10);
         });
-
-        it('should not throw an error if the request to ' + url + 'returns a non 200 http status code and no data', function (done) {
-            var blockchainInfo;
-
-            responses[url] = {
-                statusCode: 500,
-                body: 'Internal Server Error'
-            };
-
-            blockchainInfo = new BlockchainInfo(app);
-
-            setTimeout(function () {
-                expect(blockchainInfo.toJSON()).to.be.empty;
-                done();
-            }, 50);
-        });
-
     });
 
     it('should have the title set correctly', function () {
